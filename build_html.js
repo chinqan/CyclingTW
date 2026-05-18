18f62ce933f1b32f4b6069ac1a30b24376bf35ed
chinqan <chinqan@gmail.com>
2026-05-18 12:21:50 +0800

    mermaid update

--- build_html.js
+++ build_html.js
@@ -166,7 +166,7 @@ const finalHtml = `<!DOCTYPE html>
     </div>
     
     <script type="module">
-        import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.esm.min.mjs';
+        import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
         mermaid.initialize({ startOnLoad: true });
     </script>
 </body>
